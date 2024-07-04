4956bca5683ca3a435f5d9812ad852c6d39f5809
Abdulrahman Saber <abdulrahmansaber120@gmail.com>
2024-07-04 10:49:05 +0300

    Deployment: Update configurations

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 import { showAppURLCMD } from "./utlis/helpers";
 import { authRouter, ordersRouter, categoriesRouter, couponsRouter, productsRouter, usersRouter, brandsRouter } from "./routes";
+import db from "./utlis/db";
 
 dotenv.config();
 
@@ -20,10 +21,11 @@ app.use('/api/v1', [
   categoriesRouter,
 ])
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   return res.status(200).json({
-    message: "Welcome here.",
-    status: 200
+    message: "Welcome to admin store admin dashboard",
+    info: "To start using the api head to this route: /api/v1/login",
+    status: 200,
   })
 })
 
