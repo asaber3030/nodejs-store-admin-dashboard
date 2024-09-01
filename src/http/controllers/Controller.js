83ee925f229205365da578270377e7fd97c950aa
Abdulrahman Saber <abdulrahmansaber120@gmail.com>
2024-09-01 22:34:59 +0300

    Host database to AWS & update Vercel Configs

--- src/http/controllers/Controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Controller {
-    constructor(secret) {
-        this.secret = process.env.APP_SECRET;
-    }
-}
-exports.default = Controller;
